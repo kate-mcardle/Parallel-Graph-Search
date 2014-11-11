857d7ef1f85dc2a948dc6867dce09cd0c5fd46af
kate-mcardle <kate.mca@gmail.com>
2014-11-10 19:16:09 -0600

    Added testing/timing framework to Main; finished implementation of LockFreeBFS

---
+++ src/BreadthFirstSearch/LockFreeBFS.java
@@ -0,0 +1,111 @@
+package BreadthFirstSearch;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LockFreeBFS implements BreadthFirstSearch {
+	private ExecutorService neighborThreadPool;
+	private ExecutorService shortestHopThreadPool;
+	private int[] nonZeroRowCount;
+
+	private Graph graph;
+	private int[] shortest_hops;
+	private int level = 0;
+	private Queue<Integer> current;
+	private Queue<Integer> next;
+
+	public LockFreeBFS(Graph graph) {
+		this.graph = graph;
+		shortest_hops = new int[graph.n_nodes];
+		this.current = new ConcurrentLinkedQueue<Integer>();
+		this.next = new ConcurrentLinkedQueue<Integer>();
+		this.nonZeroRowCount = new int[graph.n_nodes]; 
+		for (int i = 0; i < graph.n_nodes; i++) { // TODO : see if making this parallel improves performance
+			int nnz = 0;
+			for (int j = 0; j < graph.n_nodes; j++) {
+				if (graph.adjacency_matrix[i][j] != 0) {
+					nnz++;
+				}
+			}
+			nonZeroRowCount[i] = nnz;
+		}
+	}
+
+	@Override
+	public int[] search(int source) {
+		// Algorithm from Ole Miss paper:
+		// http://cs.olemiss.edu/heroes/papers/bfs.pdf
+
+		for (int i = 0; i < graph.n_nodes; i++) {
+			shortest_hops[i] = Integer.MAX_VALUE;
+		}
+		shortest_hops[source] = level;
+
+		next.add(source);
+		NeighborExecutor neighbors;
+		while (!next.isEmpty()) {
+			level++;
+			current = next; // all nodes at this level
+			next = new ConcurrentLinkedQueue<Integer>(); // all nodes at the next level
+			neighborThreadPool = Executors.newCachedThreadPool();
+			shortestHopThreadPool = Executors.newCachedThreadPool();
+			while (!current.isEmpty()) { // while we still have nodes to process at this level
+				int node = current.remove(); // pop the next node at this level
+				neighbors = new NeighborExecutor(node);
+				neighborThreadPool.execute(neighbors); // find this node's neighbors
+			}
+			try {
+				neighborThreadPool.shutdown(); 
+				neighborThreadPool.awaitTermination(1, TimeUnit.DAYS); // wait until we have finished launching searches of all nodes at this level
+				shortestHopThreadPool.shutdown();
+				shortestHopThreadPool.awaitTermination(1, TimeUnit.DAYS); // wait until we have finished updating all the neighbors of all the nodes at this level
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return shortest_hops;
+	}
+
+	public class NeighborExecutor implements Runnable {
+
+		int node = -1;
+
+		public NeighborExecutor(int node) {
+			this.node = node;
+		}
+
+		@Override
+		public void run() {
+			ShortestHopExecutor shortestHop;
+			for (int v = 0; v < graph.n_nodes; v++) {
+				if (graph.adjacency_matrix[node][v] != 0) { // for each neighbor of this node
+					shortestHop = new ShortestHopExecutor(v);
+					shortestHopThreadPool.execute(shortestHop); // evaluate that neighbor's shortest hops from source
+				}
+			}
+		}
+
+	}
+
+	public class ShortestHopExecutor implements Runnable {
+		int v = -1;
+
+		public ShortestHopExecutor(int v) {
+			this.v = v;
+		}
+
+		@Override
+		public void run() {
+			if (shortest_hops[v] == Integer.MAX_VALUE) { // if we haven't discovered this node yet
+				next.add(v); // add it to the queue of nodes to investigate
+				shortest_hops[v] = level; // update its shortest_hops
+			}
+		}
+
+	}
+
+}
--- src/BreadthFirstSearch/Main.java
+++ src/BreadthFirstSearch/Main.java
@@ -1,15 +1,37 @@
 package BreadthFirstSearch;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
-		Graph g = new Graph(0.5, 5);
-		BreadthFirstSearch bfs = new SequentialBFS(g);
-		System.out.println(g);
-		int[] shortest_hops = bfs.search(0);
-		System.out.println("BFS results:\n");
-		for (int i = 0; i < shortest_hops.length; i++) {
-			System.out.println(shortest_hops[i]);
+		long t = System.nanoTime();
+		Graph g = new Graph(0.5, 10000);
+		long t_graph = System.nanoTime() - t;
+		System.out.println("Time to build graph = " + t_graph + " nanoseconds");
+		BreadthFirstSearch bfs_seq = new SequentialBFS(g);
+//		System.out.println(g);
+		t = System.nanoTime();
+		int[] shortest_hops_seq = bfs_seq.search(0);
+		long t_seq = System.nanoTime() - t;
+		System.out.println("Time for sequential search = " + t_seq + " nanoseconds");
+		if (evaluate_search(shortest_hops_seq, "lock-free", g, 0)) {
+			System.out.println("match!");
+		} else {
+			System.out.println("Bug!! Not a match");
 		}
+	}
+	
+	public static boolean evaluate_search(int[] shortest_hops, String type, Graph g, int source) {
+		System.out.println("Evaluating " + type + " BFS");
+		BreadthFirstSearch bfs = null;
+		if (type.equals("lock-free")) {
+			bfs = new LockFreeBFS(g);
+		}
+		long t = System.nanoTime();
+		int[] shortest_hops_parallel = bfs.search(source);
+		long t_par = System.nanoTime() - t;
+		System.out.println("Time for parallel search = " + t_par + " nanoseconds");
+		return (Arrays.equals(shortest_hops, shortest_hops_parallel));
 	}
 
 }
