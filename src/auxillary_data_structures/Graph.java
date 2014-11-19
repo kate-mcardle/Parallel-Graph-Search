985832a4ecf27ae19651228c06ed2e2a8142130b
Yamini Gotimukul <yamini485@gmail.com>
2014-11-19 12:20:26 -0600

    Adding the Graph.java to build a graph by reading from files and BFS Main.java

--- src/auxillary_data_structures/Graph.java
+++ src/auxillary_data_structures/Graph.java
@@ -1,5 +1,9 @@
 package auxillary_data_structures;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,13 +17,13 @@ import java.util.Set;
 
 public class Graph {
 	
-	public Map<Integer, Set<Edge>> adjacencyList;  
+	public Map<Integer, Set<Edge>> adjacencyList;
 	public int n_nodes;
-	public int n_edges;
-	
+	//public int n_edges;
+
 	public Graph(int n_nodes) {
 		this.n_nodes = n_nodes;
-		n_edges = 0;
+		//n_edges = 0;
 		adjacencyList = new HashMap<Integer, Set<Edge>>();
 		for (int i = 0; i < n_nodes; i++) {
 			adjacencyList.put(i, new HashSet<Edge>());
@@ -28,11 +32,11 @@ public class Graph {
 
 	public Graph(double density, int n_nodes) {
 		this.n_nodes = n_nodes;
-		adjacencyList = new HashMap<Integer,Set<Edge>>();
+		adjacencyList = new HashMap<Integer, Set<Edge>>();
 		for (int i = 0; i < n_nodes; i++) {
 			adjacencyList.put(i, new HashSet<Edge>());
 		}
-		n_edges = (int) (density * n_nodes * (n_nodes - 1));
+		int n_edges = (int) (density * n_nodes * (n_nodes - 1));
 		int count = 0;
 		int i, j;
 		Random rgen = new Random();
@@ -42,17 +46,61 @@ public class Graph {
 			if (i == j) {
 				continue;
 			}
-			Edge e = new Edge(i,j,Math.random());
+			Edge e = new Edge(i, j, Math.random());
 			boolean wasAdded = adjacencyList.get(i).add(e);
-			if(wasAdded){
+			if (wasAdded) {
 				count++;
 			}
-			
+
 		}
 	}
-	
-	public Graph(String graph_file) {
+
+	/*
+	 * BufferedReader br = new BufferedReader(new FileReader("file.txt")); try {
+	 * StringBuilder sb = new StringBuilder(); String line = br.readLine();
+	 * 
+	 * while (line != null) { sb.append(line);
+	 * sb.append(System.lineSeparator()); line = br.readLine(); } String
+	 * everything = sb.toString(); } finally { br.close(); }
+	 */
+	public Graph(String graph_file, boolean isZeroBased,int size) {
 		// read data from file, store in adjacencyList
+		adjacencyList = new HashMap<Integer, Set<Edge>>();
+		for(int i =0; i<size;i++){
+			adjacencyList.put(i, new HashSet<Edge>());
+		}
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(graph_file));
+		//	StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			long start = System.currentTimeMillis();
+			int i =-1;
+			int j =-1;
+			while (line != null){
+				String[] s = line.split("\t");
+				if(isZeroBased){
+					i = Integer.valueOf(s[0].trim());
+				    j = Integer.valueOf(s[1].trim());
+				}else{
+					i = Integer.valueOf(s[0].trim())-1;
+				    j = Integer.valueOf(s[1].trim())-1;
+				}
+				Edge e = new Edge(i,j,Math.random());
+				/*if(!adjacencyList.containsKey(i)){
+					adjacencyList.put(i, new HashSet<Edge>());
+				}*/
+				adjacencyList.get(i).add(e);
+				line = br.readLine();
+			}
+			n_nodes = size;
+			System.out.println(" Time in ms "+ String.valueOf(System.currentTimeMillis() - start));
+			//System.out.println(sb.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean add_edge(Edge e) {
@@ -60,7 +108,14 @@ public class Graph {
 	}
 
 	public static void main(String[] args) {
-		Graph g = new Graph(0.4, 5);
-		System.out.println(" Graph " + g.adjacencyList);
+		// Graph g = new Graph(0.4, 5);
+		Graph g = new Graph("src/data/soc-pokec-relationships.txt", false,1632803);
+		System.out.println("size of soc-pokec "+g.adjacencyList.size());
+		Graph g_p = new Graph("src/data/wiki-Talk.txt", true,2394385);
+		System.out.println("size of wiki-talk "+g_p.adjacencyList.size());
+		Graph g_s = new Graph("src/data/as-skitter.txt", true,1696415);
+		System.out.println("size of skitter "+g_s.adjacencyList.size());
+	/*	Graph g1 = new Graph("src/test.txt",false,40);
+		 System.out.println(" Graph " + g1.adjacencyList);*/
 	}
 }
--- src/breadth_first_search/Main.java
+++ src/breadth_first_search/Main.java
@@ -12,9 +12,91 @@ public class Main {
 		String[] search_types = { "array-locked", "lock-free" };
 		// (graph densities = 0.000015 ,graph nodes = 10000, 100000,1000000 ),
 		// (graph densities = 0.001, graph nodes = 10000, 100000)
-		double[] graph_density = { 0.015, 0.0001 };
-		int[] num_nodes = { 10000, 100000 };
+		//double[] graph_density = { 0.015, 0.0001 };
+		//int[] num_nodes = { 10000, 100000 };
+		String[] paths = {"src/data/soc-pokec-relationships.txt","src/data/wiki-Talk.txt","src/data/as-skitter.txt"};
+		int[] graph_sizes ={1632803,2394385,1696415};
+		boolean[] isZeroIndexed = {false, true,true};
+		
+		for(int i =0; i<3;i++){
+			long t = System.nanoTime();
+			Graph g = new Graph(paths[i],isZeroIndexed[i],graph_sizes[i]);
+			double t_graph = (System.nanoTime() - t + 0.0) / (Math.pow(10, 9));
+			double t_seq = 0.0;
+			int[] shortest_hops_seq = null;
+			int[] shortest_hops_parallel = null;
 
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+			//System.out.println("Graph density " + graph_density[h] + "  Graph Size " + num_nodes[h] );
+			System.out.println("Time to build graph = " + t_graph + " seconds");
+
+			// Executing Sequential Algorithm 10 times
+			for (int k = 0; k < 10; k++) {
+				// Sequential Algorithm Execution
+				BreadthFirstSearch bfs_seq = new SequentialBFS(g);
+				t = System.nanoTime();
+				shortest_hops_seq = bfs_seq.search(2);
+				t_seq += (System.nanoTime() - t + 0.0) / (Math.pow(10, 9));
+			}
+			System.out.println("Time for sequential search = " + t_seq / 10 + " seconds");
+
+			// Parallel Algorithm Execution
+			System.out.println("Approach: Collecting Task results at each level: ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			for (String type : search_types) { // Algorithm types
+				System.out.println(" ---------------------------------------------------");
+				System.out.println("Search Algorithm " + type);
+
+				for (int j = 1; j <= 7; j++) {// Number of threads
+					System.out.println("# Thread(s) " + j);
+					double t_par = 0.0;
+
+					for (int k = 0; k < 10; k++) { // Averaging the execution
+													// time
+						BreadthFirstSearch bfs_parallel = new ParallelBFS_task(g, type, j);
+						long t_par_start = System.nanoTime();
+						shortest_hops_parallel = bfs_parallel.search(2);
+						t_par += (System.nanoTime() - t_par_start + 0.0) / (Math.pow(10, 9));
+					}
+					System.out.println("Time = " + t_par / 10 + " seconds");
+
+					if (!Arrays.equals(shortest_hops_seq, shortest_hops_parallel)) {
+						System.out.println("Bug!! Not a match");
+					}
+
+				}
+				System.out.println(" ---------------------------------------------------");
+			}
+			System.out.println("Approach: Restarting thread pool at each level: ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			// Parallel Algorithm Execution - old approach (restarting thread pool at each level)
+			for (String type : search_types) { // Algorithm types
+				System.out.println(" ---------------------------------------------------");
+				System.out.println("Search Algorithm " + type);
+
+				for (int j = 1; j <= 7; j++) {// Number of threads
+					System.out.println("# Thread(s) " + j);
+					double t_par = 0.0;
+
+					for (int k = 0; k < 10; k++) { // Averaging the execution
+													// time
+						BreadthFirstSearch bfs_parallel = new ParallelBFS(g, type, j);
+						long t_par_start = System.nanoTime();
+						shortest_hops_parallel = bfs_parallel.search(2);
+						t_par += (System.nanoTime() - t_par_start + 0.0) / (Math.pow(10, 9));
+					}
+					System.out.println("Time = " + t_par / 10 + " seconds");
+
+					if (!Arrays.equals(shortest_hops_seq, shortest_hops_parallel)) {
+						System.out.println("Bug!! Not a match");
+					}
+
+				}
+				System.out.println(" ---------------------------------------------------");
+			}
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		
+		}
+		
+	/*	
 		for (int h = 1; h < 2; h++) {// each graph
 
 			long t = System.nanoTime();
@@ -92,7 +174,7 @@ public class Main {
 			}
 			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		}
-
+*/
 	}
 
 	// Not used in the main method need to remove it while submitting the code
